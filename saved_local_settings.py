28063857ca6c52b91051b289b27c75709bd2bf7d
Mike Gering <git@mfg.ferree-gering.com>
2022-07-07 11:58:31 -0400

    settings

---
+++ saved_local_settings.py
@@ -0,0 +1,6 @@
+TEMPLATES_AUTO_RELOAD = True
+DEFAULT_DOWNLOAD_DIR = 'yt'
+DEFAULT_DOWNLOAD_NAME_PATTERN = '%(title)s.%(ext)s'
+FFMPEG_LOCATION = '/usr/bin/ffmpeg'
+DB_LOCATION = 'yt_server.db'
+DL_ROOT = 'c:\\foo\\ytdl'
