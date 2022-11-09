d91fd1517fb91201dba60976da53f397bf1e2e31
Mike Gering <mike@mfg-consulting-llc.com>
2022-11-09 13:29:10 -0500

    get-db error, imports

--- local_settings.py
+++ local_settings.py
@@ -2,5 +2,5 @@ TEMPLATES_AUTO_RELOAD = True
 DEFAULT_DOWNLOAD_DIR = 'yt'
 DEFAULT_DOWNLOAD_NAME_PATTERN = '%(title)s.%(ext)s'
 FFMPEG_LOCATION = '/usr/bin/ffmpeg'
-DB_LOCATION = 'yt_server.db'
+DB_LOCATION = '../yt_server.db'
 DL_ROOT = '/ytdl'
--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+import site
+site.addsitedir('youtube_dl_downloader')
+site.addsitedir('yt_dlp_downloader')
+
 from app import app
 
 if __name__ == "__main__":
